8fe00b6a50d38549166ebe58605fcd4eed7e23eb
Umair <umairapi1995@gmail.com>
2021-09-22 12:33:29 +0500

    register new models

--- Users/admin.py
+++ Users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import (UsersInfo, DoctorInfo, UsersInfoAdminView,
-                        Appointment,CurrentAppointments, Symptoms)
+                        Appointment,CurrentAppointments, Symptom, Medicine, TestResult,Disease)
 # Register your models here.
 
 
@@ -8,4 +8,7 @@ admin.site.register(UsersInfo,UsersInfoAdminView)
 admin.site.register(DoctorInfo)
 admin.site.register(Appointment)
 admin.site.register(CurrentAppointments)
-admin.site.register(Symptoms)
+admin.site.register(Symptom)
+admin.site.register(Medicine)
+admin.site.register(TestResult)
+admin.site.register(Disease)
