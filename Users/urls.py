b937fcff82d7521e8aadc84566c0983c39a4fc04
Umair <umairapi1995@gmail.com>
2021-09-22 21:09:21 +0500

    search path

--- Users/urls.py
+++ Users/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import (Home, register, profile_view, patient_profile,view_appointment,complete_appointment,
                     doctors_list_view, doctor_profile,makeAppointmentForm,attend_appointment_form,
-                    bookAppointment, all_appointments,patients)
+                    bookAppointment, all_appointments,patients, searchpage,search)
 
 urlpatterns = [
     path("",Home,name="Home"),
+    path("searchpage/",searchpage,name="searchpage"),
+    path("search/",search,name="search"),
     path("register/",register, name="register"),
     path("login/",auth_views.LoginView.as_view(template_name="users/login.html"),name="login"),
     path("logout/",auth_views.LogoutView.as_view(),name="logout"),
@@ -21,6 +23,7 @@ urlpatterns = [
     path("view/appointment/<int:id>/",view_appointment,name="view-appointment"),
     path("attend/appointmet/<int:id>/",attend_appointment_form,name="attend-appointment"),
     path("complete/appointment/<int:id>/",complete_appointment,name="complete-appointment")
+
 
 
 ]
