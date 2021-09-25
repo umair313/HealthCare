7a6674d8f6372623151f5d5cff9ada766f040db3
Umair <umairapi1995@gmail.com>
2021-09-25 08:12:32 +0500

    rename url : ajaxTest -> get-chart-data

--- Users/urls.py
+++ Users/urls.py
@@ -3,26 +3,39 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import (Home, register, profile_view, patient_profile,view_appointment,complete_appointment,
                     doctors_list_view, doctor_profile,makeAppointmentForm,attend_appointment_form,
-                    bookAppointment, all_appointments,patients, searchpage,search)
+                    bookAppointment, all_appointments,patients, searchpage,search, test,disease_chart_data)
 
 urlpatterns = [
+    # home
     path("",Home,name="Home"),
-    path("searchpage/",searchpage,name="searchpage"),
-    path("search/",search,name="search"),
+    # users
     path("register/",register, name="register"),
     path("login/",auth_views.LoginView.as_view(template_name="users/login.html"),name="login"),
     path("logout/",auth_views.LogoutView.as_view(),name="logout"),
     path("profile/",profile_view,name="profile"),
-    path("doctors/", doctors_list_view,name="doctors"),
+    
+    # patient
+    path("searchpage/",searchpage,name="searchpage"),
+    path("search/",search,name="search"),
     path("view/doctor_profile/<int:doctor_id>/",doctor_profile,name="view-doctor-profile"),
     path("make/appointment/<int:doctor_id>/",makeAppointmentForm,name = "make-Appointment-form"),
     path("book/appointment/<int:doctor_id>",bookAppointment,name="book"),
+    path("doctors/", doctors_list_view,name="doctors"),
+    
+    # doctor
+    path("attend/appointmet/<int:id>/",attend_appointment_form,name="attend-appointment"),
+    path("complete/appointment/<int:id>/",complete_appointment,name="complete-appointment"),
     path("appointments/",all_appointments,name="appointments"),
     path("patients/",patients,name="patients"),
+    # doctor ajax requests urls
+    path("data/chart/disease/",disease_chart_data,name="disease-chart-data"),
+    
+    # common in patient and doctor
     path("patient/profile/<int:patient_id>/",patient_profile,name="patient-profile"),
     path("view/appointment/<int:id>/",view_appointment,name="view-appointment"),
-    path("attend/appointmet/<int:id>/",attend_appointment_form,name="attend-appointment"),
-    path("complete/appointment/<int:id>/",complete_appointment,name="complete-appointment")
+
+    #  for ajax test
+    path("test/",test,name="test"),
 
 
 
