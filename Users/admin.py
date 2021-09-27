18ba3b864bb8b565785bbc887d0f7869481d21c1
Umair <umairapi1995@gmail.com>
2021-09-27 10:19:21 +0500

    remove appointment models from register

--- Users/admin.py
+++ Users/admin.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
-from .models import (UsersInfo, DoctorInfo, UsersInfoAdminView,
-                        Appointment,CurrentAppointments, Symptom, Medicine, TestResult,Disease)
+from .models import UsersInfo, DoctorInfo, UsersInfoAdminView
+                        
 # Register your models here.
 
 
 admin.site.register(UsersInfo,UsersInfoAdminView)
 admin.site.register(DoctorInfo)
-admin.site.register(Appointment)
-admin.site.register(CurrentAppointments)
-admin.site.register(Symptom)
-admin.site.register(Medicine)
-admin.site.register(TestResult)
-admin.site.register(Disease)
