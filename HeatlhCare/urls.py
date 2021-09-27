024d0f1185850aca681b8d2bc317c353a48cbebc
Umair <umairapi1995@gmail.com>
2021-09-27 10:46:45 +0500

    name admin

--- HeatlhCare/urls.py
+++ HeatlhCare/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls,name="admin"),
     path("",include('Users.urls')),
     path("",include('Appointments.urls')),
     
