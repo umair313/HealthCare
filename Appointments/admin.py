f65f22984f7898cd4c5d81b2c408b07c46ac6509
Umair <umairapi1995@gmail.com>
2021-09-27 10:21:51 +0500

    Register appointment models

--- Appointments/admin.py
+++ Appointments/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import (Appointment,CurrentAppointments, 
+                        Symptom, Medicine, TestResult,Disease)
 # Register your models here.
+
+admin.site.register(Appointment)
+admin.site.register(CurrentAppointments)
+admin.site.register(Symptom)
+admin.site.register(Medicine)
+admin.site.register(TestResult)
+admin.site.register(Disease)
