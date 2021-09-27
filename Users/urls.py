e459c7ba6bacdc6f7fe92e639f936aa3e33ef709
Umair <umairapi1995@gmail.com>
2021-09-27 10:14:09 +0500

    remove appointment views urls

--- Users/urls.py
+++ Users/urls.py
@@ -1,11 +1,9 @@
 from os import name
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import (Home, register, profile_view, patient_profile,view_appointment,
-                        pending_appointment,complete_appointment,doctors_list_view,
-                        doctor_profile,makeAppointmentForm,attend_appointment_form,
-                        bookAppointment, all_appointments,patients, searchpage,search, 
-                        test,disease_chart_data, get_appointments_by_month)
+from .views import (Home, register, profile_view, patient_profile,
+                        doctors_list_view,doctor_profile,patients,
+                        searchpage,search)
 
 urlpatterns = [
     # home
@@ -20,27 +18,14 @@ urlpatterns = [
     path("searchpage/",searchpage,name="searchpage"),
     path("search/",search,name="search"),
     path("view/doctor_profile/<int:doctor_id>/",doctor_profile,name="view-doctor-profile"),
-    path("make/appointment/<int:doctor_id>/",makeAppointmentForm,name = "make-Appointment-form"),
-    path("book/appointment/<int:doctor_id>",bookAppointment,name="book"),
     path("doctors/", doctors_list_view,name="doctors"),
     
     # doctor
-    path("attend/appointmet/<int:id>/",attend_appointment_form,name="attend-appointment"),
-    path("complete/appointment/<int:id>/",complete_appointment,name="complete-appointment"),
-    path("appointments/",all_appointments,name="appointments"),
+
     path("patients/",patients,name="patients"),
-    # doctor ajax requests urls
-    path("data/chart/disease/",disease_chart_data,name="disease-chart-data"),
-    path("data/chart/appointments",get_appointments_by_month,name="month-appointment"),
+
     # common in patient and doctor
     path("patient/profile/<int:patient_id>/",patient_profile,name="patient-profile"),
-    path("view/appointment/<int:id>/",view_appointment,name="view-appointment"),
-    
-    # ajax request for pending apoointment count
-    path("appointment/pending/",pending_appointment,name="pending-appointment"),
-    #  for ajax test
-    path("test/",test,name="test"),
-
 
 
 ]
